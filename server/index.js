0c0b2b306e6d25cc7111524106a48a640efe6f81
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2024-02-28 12:38:35 +0100

    Frontend website ready

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-require("dotenv").config();
-
-//Starting up server
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.clear();
-  console.log(`Running on port ${PORT}.`);
-});
--- server/routes/email-service.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { sendEmail } = require("../services/email-service");
-
-
-router.route("/sendEmail").post(sendEmail);
-
-module.exports = router;
